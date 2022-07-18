3b947df5c16809ebe1231422fb45473884a81949
Gianpierre Fernandez <fgianpierre@gmail.com>
2022-07-18 23:04:03 +0200

    add swiper and instagram API

---
+++ components/InstagramFeed/InstagramFeed.tsx
@@ -0,0 +1,83 @@
+import React, {useEffect, useState} from 'react'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import {Navigation, Pagination, EffectFade} from 'swiper'
+
+import Image from 'next/image'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+export const InstagramFeed = () => {
+  const [feeds, setFeed] = useState([])
+
+  useEffect(() => {
+    const getFeeds = async () => {
+      const response = await fetch(
+        `https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_ACCESS_TOKEN}`,
+      )
+      const data = await response.json()
+      setFeed(data.data)
+    }
+
+    getFeeds()
+  }, [])
+
+  const images = feeds.filter(feed => feed.media_type === 'IMAGE')
+
+  return (
+    <>
+      <h3>InstagramFeed @gianpitri</h3>
+      <Swiper
+        modules={[Navigation, Pagination, EffectFade]}
+        spaceBetween={50}
+        slidesPerView={1}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        navigation
+        pagination={{clickable: true}}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={swiper => console.log(swiper)}
+      >
+        {images.map(image => (
+          <SwiperSlide key={image.id}>
+            <div className="image-container">
+              <Image
+                src={image.media_url}
+                alt={image.caption}
+                layout="responsive"
+                height={400}
+                width={250}
+              />
+              <div className="image-caption">
+                <p>{image.caption}</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <style jsx>{`
+        .image-container {
+          position: relative;
+        }
+
+        .image-caption {
+          display: flex;
+          justify-content: center;
+          margin-top: 20px;
+        }
+      `}</style>
+    </>
+  )
+}
