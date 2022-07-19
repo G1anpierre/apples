cf5dd6fe06db5fe30aaef83db96eb7014fa1b786
Gianpierre Fernandez <fgianpierre@gmail.com>
2022-07-19 21:22:31 +0200

    use switch statement

--- components/AllSwiper/AllSwiper.tsx
+++ components/AllSwiper/AllSwiper.tsx
@@ -8,10 +8,48 @@ import 'swiper/css/pagination'
 
 export const AllSwiper = ({feeds}) => {
   const ChooseElementToRender = feed => {
-    if (feed.media_type === 'IMAGE') {
-      return (
-        <>
-          <div className="image-container">
+    switch (feed.media_type) {
+      case 'IMAGE':
+        return (
+          <>
+            <div className="image-container">
+              <Image
+                src={feed.media_url}
+                alt={feed.caption}
+                layout="responsive"
+                height={640}
+                width={360}
+                objectFit="cover"
+              />
+            </div>
+          </>
+        )
+      case 'VIDEO':
+        return (
+          <>
+            <div className="video-container">
+              <iframe src={feed.media_url} className="iframe" frameBorder={0} />
+            </div>
+            <style jsx>{`
+              .video-container {
+                position: relative;
+                padding-bottom: 177%; /* 16:9 */
+                height: 0;
+              }
+
+              .iframe {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+              }
+            `}</style>
+          </>
+        )
+      case 'CAROUSEL_ALBUM':
+        return (
+          <div className="carrousel-container">
             <Image
               src={feed.media_url}
               alt={feed.caption}
@@ -21,103 +59,8 @@ export const AllSwiper = ({feeds}) => {
               objectFit="cover"
             />
           </div>
-          <style jsx>{``}</style>
-        </>
-      )
+        )
     }
-    if (feed.media_type === 'VIDEO') {
-      return (
-        <>
-          <div className="video-container">
-            {/* <figure>
-              <video controls className="video" width="250" height="400">
-                <source src={feed.media_url} />
-              </video>
-            </figure> */}
-            <iframe src={feed.media_url} className="iframe" frameBorder={0} />
-          </div>
-          <style jsx>{`
-            .video-container {
-              position: relative;
-              padding-bottom: 177%; /* 16:9 */
-              height: 0;
-            }
-
-            .iframe {
-              position: absolute;
-              top: 0;
-              left: 0;
-              width: 100%;
-              height: 100%;
-            }
-          `}</style>
-        </>
-      )
-    }
-    if (feed.media_type === 'CAROUSEL_ALBUM') {
-      return (
-        <div className="carrousel-container">
-          <Image
-            src={feed.media_url}
-            alt={feed.caption}
-            layout="responsive"
-            height={640}
-            width={360}
-            objectFit="cover"
-          />
-        </div>
-      )
-    }
-
-    // switch (feed) {
-    //   case 'IMAGE':
-    //     return (
-    //       <>
-    //         <div className="image-container">
-    //           <Image
-    //             src={feed.media_url}
-    //             alt={feed.caption}
-    //             layout="responsive"
-    //             height={400}
-    //             width={250}
-    //             objectFit="cover"
-    //           />
-    //           <div className="image-caption">
-    //             <p>{feed.caption}</p>
-    //           </div>
-    //         </div>
-    //       </>
-    //     )
-    //   case 'VIDEO':
-    //     return (
-    //       <>
-    //         <div className="video-container">
-    //           <video controls>
-    //             <source src={feed.media_url} />
-    //           </video>
-    //           <div className="video-captio">
-    //             <p>{feed.caption}</p>
-    //           </div>
-    //         </div>
-    //       </>
-    //     )
-    //   case 'CAROUSEL_ALBUM':
-    //     return (
-    //       <div className="carrousel-container">
-    //         <Image
-    //           src={feed.media_url}
-    //           alt={feed.caption}
-    //           layout="responsive"
-    //           height={400}
-    //           width={250}
-    //           objectFit="cover"
-    //         />
-    //         <div className="carrousel-caption">
-    //           <p>{feed.caption}</p>
-    //         </div>
-    //       </div>
-    //     )
-    // }
   }
 
   return (
